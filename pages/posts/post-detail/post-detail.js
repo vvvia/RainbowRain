872e181a246b3433973a0d51a98463f1db64be84
tyler <1291265162@qq.com>
2019-03-25 20:50:52 +0800

    3rd commit

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,8 +1,8 @@
 var postsData = require('../../../data/posts-data.js')
 
 Page({
-  data:{
-
+  data: {
+    isPlayingMusic: false
   },
 
   onLoad: function(option) {
@@ -22,33 +22,53 @@ Page({
       postsCollected[postId] = false;
       wx.setStorageSync('posts_collected', postsCollected);
     }
-        if (postsCollected) {
-            var postCollected = postsCollected[postId];
-            if (postCollected){
-              this.setData({
-                collected: postCollected
-              })
-            }
-        }
-        
-        else {
-            var postsCollected = {};
-            postsCollected[postId] = false;
-            wx.setStorageSync('posts_collected', postsCollected);
-        }
+    if (postsCollected) {
+      var postCollected = postsCollected[postId];
+      if (postCollected) {
+        this.setData({
+          collected: postCollected
+        })
+      }
+    } else {
+      var postsCollected = {};
+      postsCollected[postId] = false;
+      wx.setStorageSync('posts_collected', postsCollected);
+    }
 
   },
 
-  onColletionTap: function (event) {
-    // this.getPostsCollectedSyc();
-    this.getPostsCollectedAsy();
+  onColletionTap: function(event) {
+    this.getPostsCollectedSyc();
+    //this.getPostsCollectedAsy();
   },
 
-  getPostsCollectedAsy: function () {
+  onMusicTap: function(event) {
+    var isPlayingMusic = this.data.isPlayingMusic;
+    var currentPostId = this.data.postId;
+    var postData = postsData.postList[currentPostId];
+    if (isPlayingMusic) {
+      wx.pauseBackgroundAudio();
+      this.setData({
+        isPlayingMusic : false
+      })
+    } else {
+      wx.playBackgroundAudio({
+        url: postData.music.url,
+        title: postData.music.title,
+        coverImg: postData.music.coverImg,
+      })
+      this.setData({
+        isPlayingMusic : true
+      })
+    }
+
+  },
+
+  getPostsCollectedAsy: function() {
     var that = this;
     wx.getStorage({
       key: "posts_collected",
-      success: function (res) {
+      success: function(res) {
         var postsCollected = res.data;
         var postCollected = postsCollected[that.data.currentPostId];
         // 收藏变成未收藏，未收藏变成收藏
@@ -57,40 +77,19 @@ Page({
         that.showToast(postsCollected, postCollected);
       }
     })
-  },
+  }, //异步
 
-  getPostsCollectedSyc: function () {
+  getPostsCollectedSyc: function() {
     var postsCollected = wx.getStorageSync('posts_collected');
     var postCollected = postsCollected[this.data.currentPostId];
     // 收藏变成未收藏，未收藏变成收藏
     postCollected = !postCollected;
     postsCollected[this.data.currentPostId] = postCollected;
     this.showToast(postsCollected, postCollected);
-  },
+  }, //同步
 
-  showModal: function (postsCollected, postCollected) {
-    var that = this;
-    wx.showModal({
-      title: "收藏",
-      content: postCollected ? "收藏该文章？" : "取消收藏该文章？",
-      showCancel: "true",
-      cancelText: "取消",
-      cancelColor: "#333",
-      confirmText: "确认",
-      confirmColor: "#405f80",
-      success: function (res) {
-        if (res.confirm) {
-          wx.setStorageSync('posts_collected', postsCollected);
-          // 更新数据绑定变量，从而实现切换图片
-          that.setData({
-            collected: postCollected
-          })
-        }
-      }
-    })
-  },
 
-  showToast: function (postsCollected, postCollected) {
+  showToast: function(postsCollected, postCollected) {
     // 更新文章是否的缓存值
     wx.setStorageSync('posts_collected', postsCollected);
     // 更新数据绑定变量，从而实现切换图片
@@ -104,7 +103,27 @@ Page({
     })
   },
 
+  onShareTap: function(event) {
+    var itemList = [
+      "分享给微信好友",
+      "分享到朋友圈",
+      "分享到QQ",
+      "分享到微博"
+    ]
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor: "#405f80",
+      success: function(res) {
+        //res.cancel;
+        //res.tapIndex;//数组元素 从0开始
+        wx.showModal({
+          title: '用户' + itemList[res.tapIndex],
+          content: "用户是否取消？" + res.cancel + "现在好像不支持分享，什么时候支持呢？",
 
+        })
+      }
+    })
+  }
 
 
 })
